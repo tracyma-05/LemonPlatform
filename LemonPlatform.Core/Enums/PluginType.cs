5c26d808ce2d1db26cb5076ec0f9420f13d8dab7
Tracy Ma <zhongbin_ma@outlook.com>
2024-11-29 14:50:28 +0800

    group

--- LemonPlatform.Core/Enums/PluginType.cs
+++ LemonPlatform.Core/Enums/PluginType.cs
@@ -1,28 +1,13 @@
-﻿using System.ComponentModel;
-
-namespace LemonPlatform.Core.Enums
+﻿namespace LemonPlatform.Core.Enums
 {
     public enum PluginType
     {
-        [Description("Data generation")]
         DataGeneration,
-
-        [Description("Convert tools")]
         ConverterTools,
-
-        [Description("Text convert & handler")]
         TextTools,
-
-        [Description("Json handler tools")]
         JsonTools,
-
-        [Description("Image handler tools")]
         ImageTools,
-
-        [Description("Data structure learning")]
         DataStructures,
-
-        [Description("Else tools")]
         Else
     }
 }
