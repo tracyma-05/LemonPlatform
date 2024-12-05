51665652b9dab871213e036d9207e94d697b1049
Tracy Ma <zhongbin_ma@outlook.com>
2024-12-05 13:49:17 +0800

    fix

--- LemonPlatform.Module.DataStructure/Views/SkipListView.xaml.cs
+++ LemonPlatform.Module.DataStructure/Views/SkipListView.xaml.cs
@@ -1,13 +1,22 @@
 ﻿using LemonPlatform.Core.Infrastructures.Denpendency;
+using LemonPlatform.Module.DataStructure.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace LemonPlatform.Module.DataStructure.Views
 {
     public partial class SkipListView : Page, ITransientDependency
     {
-        public SkipListView()
+        public SkipListView(SkipListViewModel model)
         {
             InitializeComponent();
+            DataContext = model;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var context = (SkipListViewModel)DataContext;
+            context.Input = string.Join(',', context.Render.Keys);
         }
     }
 }
